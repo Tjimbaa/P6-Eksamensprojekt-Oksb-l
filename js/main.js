aaa0c91853330ffc01494c1e620980d0e9b3650a
lass842k <lassebaikjepsen@gmail.com>
2019-05-22 12:57:32 +0200

    Javascript, quote slideshow

--- js/main.js
+++ js/main.js
@@ -21,3 +21,27 @@ function scrollAppear2() {
 }
 
 window.addEventListener("scroll", scrollAppear2);
+
+
+// SLIDESHOW QUOTES //
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(n) {
+   var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+      slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
