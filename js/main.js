537dd2558bf25813134a6b3666382948bdc502ce
Tj <thinh_tran_4@msn.com>
2019-05-22 12:49:05 +0200

    javascript transition

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-test
+function scrollAppear() {
+  var introText = document.querySelector(".infoBox");
+  var introPosition = introText.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.5;
+
+  if (introPosition < screenPosition) {
+    introText.classList.add("test-appear");
+  }
+}
+
+window.addEventListener("scroll", scrollAppear);
+
+function scrollAppear2() {
+  var introText = document.querySelector(".infoBox2");
+  var introPosition = introText.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.5;
+
+  if (introPosition < screenPosition) {
+    introText.classList.add("test-appear1");
+  }
+}
+
+window.addEventListener("scroll", scrollAppear2);
